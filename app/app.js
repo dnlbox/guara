9a9f2cc80bc98a72ddba93915dd7855dd6d5d3b7
dnlbox <dnl.vzlt@gmail.com>
2017-02-18 18:02:08 -0500

    enhancements and login google

--- app/app.js
+++ app/app.js
@@ -6,9 +6,27 @@ var app = new Vue({
     message: 'Loading...'
   },
   methods: {
-    compute: function(message) { console.log(message) }
+    onSignOut: function () {
+      this.auth2 = gapi.auth2.getAuthInstance()
+      this.auth2.signOut().then( () => {
+        this.auth2.disconnect()
+        console.log('User signed out.')
+      })
+    }
   }
 })
 
-console.log('loading...');
+function onSignIn(googleUser) {
+  var profile = googleUser.getBasicProfile()
+  console.log('Id: ' + profile.getId())  
+  console.log('Name: ' + profile.getName());
+  console.log('Image URL: ' + profile.getImageUrl());
+  console.log('Email: ' + profile.getEmail());
+  console.log('Token: ' + googleUser.getAuthResponse().id_token);
+}
 
+function onSignOut() {
+  var auth2 = gapi.auth2.getAuthInstance();
+  auth2.signOut();
+  auth2.disconnect();
+}
--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,37 @@
+/**
+ *
+ *  Gulp Aurora
+ *  Boilerplate for Web Development
+ *  Note: For Guara app,
+ *        using Vue.js and Bulma css
+ *
+ *  Daniel Leite (2017)
+ *  MIT License;
+ *
+ */
 'use strict';
 
-var gulp    = require('gulp');
-var connect = require('gulp-connect');
-var pug     = require('gulp-pug');      // new name of Jade
-var changed = require('gulp-changed');
-var babel   = require('gulp-babel');
-var sass    = require('gulp-sass');
-var size    = require('gulp-size');
-var plumber = require('gulp-plumber');
-var util    = require('gulp-util');
+// Gulp and Plugin definitions
+var gulp     = require('gulp');
+var connect  = require('gulp-connect');
+var pug      = require('gulp-pug'); // new name of Jade
+var changed  = require('gulp-changed');
+var babel    = require('gulp-babel');
+var sass     = require('gulp-sass');
+var size     = require('gulp-size');
+var plumber  = require('gulp-plumber');
+var util     = require('gulp-util');
+var imagemin = require('gulp-imagemin');
 
+// Error handler
 var onError = function (err) {
   util.beep();
   util.log(util.colors.yellow(err.message));
 };
 
-// HTML
-gulp.task('pug', function() {
+// Generate HTML from PUG (Jade)
+// If Pug is not necessary, 
+gulp.task('html', function() {
   return gulp.src('app/**/*.pug')
     .pipe(plumber({ errorHandler: onError }))
     .pipe(changed('build', {extension:'.html'}))
@@ -25,7 +40,7 @@ gulp.task('pug', function() {
     .pipe(connect.reload());
 })
 
-// Javascript
+// Process Javascript
 gulp.task('js', function() {
   return gulp.src('app/**/*.js')
     .pipe(plumber({ errorHandler: onError }))
@@ -36,12 +51,15 @@ gulp.task('js', function() {
     .pipe(connect.reload());
 })
 
+// Load Vue.js from node module
+// TODO: consider change to vendor, js/libs or even
+//       add as source on js task
 gulp.task('vue', function() {
   return gulp.src('node_modules/vue/dist/vue.js')
     .pipe(gulp.dest('build/js'));
 })
 
-// CSS
+// Process CSS
 gulp.task('scss', function() {
   return gulp.src('app/**/*.scss')
     .pipe(plumber({ errorHandler: onError }))
@@ -51,22 +69,30 @@ gulp.task('scss', function() {
     .pipe(connect.reload());
 })
 
+// Load Bulma css framework from node module
+// Note: it will load both bulma.css and bulma.css.map
+// TODO: consider add to Process CSS or change to 
+//       css/libs css/vendor
 gulp.task('bulma', function() {
-  return gulp.src('node_modules/bulma/css/bulma.css')
+  return gulp.src('node_modules/bulma/css/bulma.*')
     .pipe(gulp.dest('build/css'));
 })
 
-// Images
+// Process Images
 gulp.task('images', function() {
   return gulp.src('app/images/**/*')
     .pipe(plumber({ errorHandler: onError }))
+    .pipe(imagemin({
+      progressive: true,
+      interlaced: true
+    }))
     .pipe(gulp.dest('build/images'))
     .pipe(size({title: 'images'}));
 })
 
 // Development server setup 
 gulp.task('watch', function() {
-  gulp.watch('app/**/*.pug', ['pug']);
+  gulp.watch('app/**/*.pug', ['html']);
   gulp.watch('app/**/*.js', ['js']);
   gulp.watch('app/**/*.scss', ['scss']);
   gulp.watch('app/images/**/*', ['images']);  
@@ -74,6 +100,7 @@ gulp.task('watch', function() {
 
 gulp.task('connect', function() {
   connect.server({
+    host:'guaraproject.com',
     port:3000,
     root:['build'],
     livereload:true
@@ -81,8 +108,8 @@ gulp.task('connect', function() {
 })
 
 // build the solution
-gulp.task('default', ['pug', 'js', 'vue', 'scss', 'bulma', 'images']);
+gulp.task('default', ['html', 'js', 'vue', 'scss', 'bulma', 'images']);
 
 // open server for development
-gulp.task('serve', ['pug', 'js', 'vue', 'scss', 'bulma', 'images', 'watch', 'connect']);
+gulp.task('serve', ['html', 'js', 'vue', 'scss', 'bulma', 'images', 'watch', 'connect']);
 
